c8501a02493f96aed0c7dbb0781a91f31f702727
Maulana <maulanaridwan87@gmail.com>
2026-06-02 11:52:53 +0700

    Fix auto-reload bug by touching clients.conf before SIGHUP

--- radius-server/entrypoint.sh
+++ radius-server/entrypoint.sh
@@ -6,6 +6,8 @@
   while true; do
     printf "HTTP/1.1 200 OK\r\nContent-Length: 15\r\n\r\nRadius Reloaded" | nc -l -p 8080 > /dev/null
     echo "[RADIUS] Reload signal received via webhook. Reloading FreeRADIUS..."
+    # Touch clients.conf untuk memaksa FreeRADIUS melakukan reload penuh (menghindari bug "No files changed")
+    touch /etc/raddb/clients.conf
     # Send HUP signal to radiusd to reload clients and configs
     killall -HUP radiusd
   done
